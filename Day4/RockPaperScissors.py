2a9d9be71955cf13bfdf3780785d223a19328c94
PAUL PROMISE <paul.promise@outlook.com>
2025-01-10 18:58:07 +0000

    added a code to break the program when user input is invalid

--- Day4/RockPaperScissors.py
+++ Day4/RockPaperScissors.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 rock = '''
     _______
@@ -30,17 +31,20 @@ scissors = '''
 #Write your code below this line 👇
 print("Welcome to the rock, paper, and scissors game")
 user_response = input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors ")
-user_response = int(user_response)
 
+if not user_response.isdigit() or int(user_response) not in [0, 1, 2]:
+    print("Invalid input! Please enter a number (0, 1, or 2).")
+    sys.exit()
+else:
+    user_response = int(user_response)
 
+# Display the user's choice
 if user_response == 0:
     print(rock)
 elif user_response == 1:
     print(paper)
-elif user_response == 2:
-    print(scissors)
 else:
-    print("Input not valid!")
+    print(scissors)
 
 #Generate random number between 0 and 2
 random_num = random.randint(0, 2)
@@ -60,8 +64,8 @@ elif user_response == 2 and random_num == 1:
     print("You Won!")
 elif user_response == 1 and random_num == 2:
     print("You loss!")
-elif user_response == 2 and random == 0:
-    print("You loss")
+elif user_response == 2 and random_num == 0:
+    print("You loss!")
 elif user_response == 0 and random_num == 2:
     print("You Won!")
 elif user_response == random_num:
