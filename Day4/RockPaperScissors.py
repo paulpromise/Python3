3919f1ebc926c924e3029490d2e23c2d0c83c9c9
PAUL PROMISE <paul.promise@outlook.com>
2025-01-10 21:01:57 +0000

    using list to print games images instead of if

--- Day4/RockPaperScissors.py
+++ Day4/RockPaperScissors.py
@@ -28,6 +28,7 @@ scissors = '''
 ---.__(___)
 '''
 
+
 #Write your code below this line 👇
 print("Welcome to the rock, paper, and scissors game")
 User_choice = input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors ")
@@ -38,24 +39,35 @@ if not User_choice.isdigit() or int(User_choice) not in [0, 1, 2]:
 else:
     User_choice = int(User_choice)
 
+
+
 # Display the user's choice
+game_image = [rock, paper, scissors]
+print(game_image[User_choice])
+
+""" 
 if User_choice == 0:
     print(rock)
 elif User_choice == 1:
     print(paper)
 else:
-    print(scissors)
+    print(scissors) """
 
 #Generate random number between 0 and 2
 print("Computer Choice")
 computer_choice = random.randint(0, 2)
 
+game_image = [rock, paper, scissors]
+print(game_image[computer_choice])
+
+
+""" 
 if computer_choice == 0:
     print(rock)
 elif computer_choice == 1:
     print(paper)
 else:
-    print(scissors)
+    print(scissors) """
 
 if User_choice == 0 and computer_choice == 1:
     print("You loss!")
