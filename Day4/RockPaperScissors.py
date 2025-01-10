745a87dba4a26f416f30b7365c578f838c56cb9b
PAUL PROMISE <paul.promise@outlook.com>
2025-01-10 20:08:36 +0000

    added a code to break the program when user input is invalid

--- Day4/RockPaperScissors.py
+++ Day4/RockPaperScissors.py
@@ -30,46 +30,46 @@ scissors = '''
 
 #Write your code below this line 👇
 print("Welcome to the rock, paper, and scissors game")
-user_response = input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors ")
+User_choice = input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors ")
 
-if not user_response.isdigit() or int(user_response) not in [0, 1, 2]:
+if not User_choice.isdigit() or int(User_choice) not in [0, 1, 2]:
     print("Invalid input! Please enter a number (0, 1, or 2). ")
     sys.exit()
 else:
-    user_response = int(user_response)
+    User_choice = int(User_choice)
 
 # Display the user's choice
-if user_response == 0:
+if User_choice == 0:
     print(rock)
-elif user_response == 1:
+elif User_choice == 1:
     print(paper)
 else:
     print(scissors)
 
 #Generate random number between 0 and 2
 print("Computer Choice")
-random_num = random.randint(0, 2)
+computer_choice = random.randint(0, 2)
 
-if random_num == 0:
+if computer_choice == 0:
     print(rock)
-elif random_num == 1:
+elif computer_choice == 1:
     print(paper)
 else:
     print(scissors)
 
-if user_response == 0 and random_num == 1:
+if User_choice == 0 and computer_choice == 1:
     print("You loss!")
-elif user_response == 1 and random_num == 0:
+elif User_choice == 1 and computer_choice == 0:
     print("You Won!")
-elif user_response == 2 and random_num == 1:
+elif User_choice == 2 and computer_choice == 1:
     print("You Won!")
-elif user_response == 1 and random_num == 2:
+elif User_choice == 1 and computer_choice == 2:
     print("You loss!")
-elif user_response == 2 and random_num == 0:
+elif User_choice == 2 and computer_choice == 0:
     print("You loss!")
-elif user_response == 0 and random_num == 2:
+elif User_choice == 0 and computer_choice == 2:
     print("You Won!")
-elif user_response == random_num:
+elif User_choice == computer_choice:
     print("Is a draw.")
 
 
