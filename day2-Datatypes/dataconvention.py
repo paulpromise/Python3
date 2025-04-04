2fcc7bec1210a8bdb745151175eecfb00a489a19
paulpromise <paul.promise@outlook.com>
2025-04-04 11:11:44 +0000

    modified data convention file

--- day2-Datatypes/dataconvention.py
+++ day2-Datatypes/dataconvention.py
@@ -1,10 +1,10 @@
 weight = input("Enter your weight in kg: ")
 height = input("Enter your height in m: ")
 bmi = (int(weight) / (float(height) ** 2))
-print(int(bmi)) 
+print(int( f"BMI:  {bmi}")) 
 
-""" age = 20;
+age = 20
 firstName = "Paul"
 LastName = "Promise"
 
-print(f"He is called {firstName} {LastName} and he is {age} years old") """
+print(f"He is called {firstName} {LastName} and he is {age} years old") 
